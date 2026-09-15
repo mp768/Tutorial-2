2669817980f76f6c558671a12bf945181f7ed018
mp768 <mp7252006@gmail.com>
2026-09-14 20:02:48 -0500

    Add game scene, add parallax, and working on columns.
    - I also fixed the rotational values for the bird.

---
+++ Assets/Autoload Scripts/GameSignals.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public partial class GameSignals : Node
+{
+    public static GameSignals Instance { get; private set; }
+
+    // NOTE: Godot has this concept of a "signal", which is essentially just a callback. For the C# version of
+    // Godot, you need to have the "EventHandler" suffix for any "signal" you want to declare. 
+    //
+    // When accessing the signal, declared as "XYZEventHandler" for example, you would use "SignalName.XYZ" to 
+    // get it's string name. To then "listen" to it and "invoke" the callback, you would use either 
+    // 
+    // "XYZ += MethodName" to set up the listener and "XYZ -= MethodName" to disconnect it
+    // or 
+    // "EmitSignal(SignalName.XYZ, ..any args go here)" to invoke the callback, calling all connected listeners.
+    //
+
+    [Signal]
+    public delegate void PlayerDiedEventHandler();
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+}
--- Assets/Scripts/FlappyBird.cs
+++ Assets/Scripts/FlappyBird.cs
@@ -14,8 +14,8 @@ public partial class FlappyBird : RigidBody2D
 
 	// NOTE: These constants are simply for doing the little "tilt" that the original
 	// flappy bird does when jumping. It just feels odd to me not having that.
-	private const float ROTATION_START = -30.0f;
-	private const float ROTATION_END = 20.0f;
+	private const float ROTATION_START = -18.0f;
+	private const float ROTATION_END = 15.0f;
 
 	public override void _Ready()
 	{
@@ -37,24 +37,30 @@ public partial class FlappyBird : RigidBody2D
 
 				// Apply the (upward) jump force to the flappy bird.
 				// NOTE: Negative Y values are up in Godot 2D.
+				// 2ND NOTE: Using the 'ApplyForce' method from 'RigidBody2D' doesn't seem
+				// to work as well as applying the force through linear velocity.
 				LinearVelocity = new Vector2(0, -jumpForce);
 
 				// Start the "tilt" of the flappy bird.
-				animatedSprite.Rotation = ROTATION_START;
+				animatedSprite.Rotation = Mathf.DegToRad(ROTATION_START);
 			}
 		}
 
-		animatedSprite.Rotation = Mathf.LerpAngle(animatedSprite.Rotation, ROTATION_END, (float)(delta * 1.5));
+		animatedSprite.Rotation = Mathf.LerpAngle(animatedSprite.Rotation, Mathf.DegToRad(ROTATION_END), (float)(delta * 1.5));
 	}
 
 	private void OnBodyEntered(Node node)
 	{
 		if (isDead) return;
 
+		GD.Print("Collided with something! ", node.GetGroups(), " ", node.Name);
+
 		if (node.IsInGroup(Constants.KILL_SURFACE_TAG))
 		{
 			isDead = true;
 			animatedSprite.Play("dead");
+
+			GameSignals.Instance.EmitSignal(GameSignals.SignalName.PlayerDied);
 		}
 	}
 }
